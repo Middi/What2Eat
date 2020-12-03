d94172ea9de4444c57730991840e6e07486f2936
Middi <richard@richardmiddleton.me>
2020-12-03 13:34:51 +0100

    Initial Commit

---
+++ What2Eat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  What2Eat
+//
+//  Created by Richard Middleton on 03/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
