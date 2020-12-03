fb828474754f6efe832933ec3bb4139aa01ffe10
Middi <richard@richardmiddleton.me>
2020-12-03 18:06:34 +0100

    first commit

--- What2Eat/ViewController.swift
+++ What2Eat/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    @IBOutlet weak var foodImage: UIImageView!
+    @IBOutlet weak var toggle: UIImageView!
+    
+    
+    let arr = [#imageLiteral(resourceName: "food-vector-free-icon-set-32"),#imageLiteral(resourceName: "food-vector-free-icon-set-25"),#imageLiteral(resourceName: "food-vector-free-icon-set-31"),#imageLiteral(resourceName: "food-vector-free-icon-set-33"),#imageLiteral(resourceName: "food-vector-free-icon-set-21"),#imageLiteral(resourceName: "food-vector-free-icon-set-39"),#imageLiteral(resourceName: "food-vector-free-icon-set-37"),#imageLiteral(resourceName: "food-vector-free-icon-set-27"),#imageLiteral(resourceName: "food-vector-free-icon-set-40"),#imageLiteral(resourceName: "food-vector-free-icon-set-22"),#imageLiteral(resourceName: "food-vector-free-icon-set-22")]
+    let vegArr = [#imageLiteral(resourceName: "food-vector-free-icon-set-32"),#imageLiteral(resourceName: "food-vector-free-icon-set-25"),#imageLiteral(resourceName: "food-vector-free-icon-set-31"),#imageLiteral(resourceName: "food-vector-free-icon-set-29"),#imageLiteral(resourceName: "food-vector-free-icon-set-23"),#imageLiteral(resourceName: "food-vector-free-icon-set-39"),#imageLiteral(resourceName: "food-vector-free-icon-set-37"),#imageLiteral(resourceName: "food-vector-free-icon-set-35"),#imageLiteral(resourceName: "food-vector-free-icon-set-40"),#imageLiteral(resourceName: "food-vector-free-icon-set-22")]
+    
+    
+    var veg = false
+    
+    func toggler() {
+        veg = !veg
+        veg ? (toggle.image = #imageLiteral(resourceName: "on")) : (toggle.image = #imageLiteral(resourceName: "off"))
     }
-
-
+    
+    @IBAction func Button(_ sender: UIButton) {
+        if(veg) {
+            foodImage.image = vegArr.randomElement()
+        }
+        else {
+            foodImage.image = arr.randomElement()
+        }
+    }
+    
+    
+    @IBAction func vegBtn(_ sender: UIButton) {
+        toggler()
+    }
+    
 }
 
